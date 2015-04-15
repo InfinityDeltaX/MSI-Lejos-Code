317e6c2e122061316168a0eafca75364530ea95e
robert <infinitydeltax@gmail.com>
2015-04-14 20:47:57 -0400

    Search functionality done.

--- LEJOS/src/auto/Search.java
+++ LEJOS/src/auto/Search.java
@@ -1,24 +1,68 @@
 package auto;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lejos.hardware.motor.Motor;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Behavior;
 
 public class Search implements Behavior{
+	
+	static boolean go = true;
 
 	@Override
 	public boolean takeControl() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public void action() {
-		
+		go = true;
+		float angle = getClosestObstacleHeading(10);
+		align(angle);
 	}
 
 	@Override
 	public void suppress() {
-		
+		go = false;
 	}
 	
+	public static float getClosestObstacleHeading(int resolution){
+		Map<Float, Float> map = scan(resolution);
+		float minDist = Float.MAX_VALUE;
+		float bestHeading = 0;
+		for(Float f : map.keySet()){
+			if(minDist > map.get(f)){
+				minDist = map.get(f);
+				bestHeading = f;
+			}
+		}
+		return bestHeading;
+	}
+
+	public static Map<Float, Float> scan(int resolution){ //res is the number of samples to take.
+		Map<Float, Float> output = new HashMap<Float, Float>();
+
+		int currentAngle = 0;
+		while(currentAngle <= 360 && go){
+			Motor.C.rotateTo(currentAngle, false);
+			float heading = Sensors.getReading(Sensors.heading);
+			float dist = Sensors.getReading(Sensors.distance);
+			output.put(heading, dist);
+		}
+		
+		return output;
+	}
 	
+	public static void align(float angle){
+		final int tolerance = 2;
+		float currentAngle = 0;
+		
+		do{
+			currentAngle = Sensors.getReading(Sensors.heading);
+			Sensors.pilot.rotate(currentAngle-angle);
+		} while(Math.abs(currentAngle-angle) >= tolerance && go);
+	}
 
 }
--- LEJOS/src/auto/Sensors.java
+++ LEJOS/src/auto/Sensors.java
@@ -1,10 +1,57 @@
 package auto;
 
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class Sensors {
 	public final static Port one;
 	public final static Port two;
 	public final static Port three;
 	public final static Port four;
+	
+	public static final SensorModes ultrasonic;
+	public static final SensorModes color = null;
+	public static final SensorModes touch1;
+	public static final SensorModes touch2;
+	public static final SensorModes gyro;
+	
+	public static final SampleProvider distance;
+	public static final SampleProvider heading;
+	public static final SampleProvider isPressed1;
+	public static final SampleProvider isPressed2;
+	
+	public static final DifferentialPilot pilot = new DifferentialPilot(3, 3, Motor.A, Motor.B);
+	
+	
+	static{
+		one = LocalEV3.get().getPort("S1");
+		two = LocalEV3.get().getPort("S2");
+		three = LocalEV3.get().getPort("S3");
+		four = LocalEV3.get().getPort("S4");
+		
+		ultrasonic = new EV3UltrasonicSensor(one);
+		//color = new EV3ColorSensor(two);
+		touch1 = new EV3TouchSensor(three);
+		touch2 = new EV3TouchSensor(four);
+		gyro = new EV3GyroSensor(two);
+		
+		distance = ultrasonic.getMode("Distance");
+		heading = gyro.getMode("Angle");
+		isPressed1 = touch1.getMode("Touch");
+		isPressed2 = touch2.getMode("Touch");
+	}
+	
+	public static float getReading(SampleProvider s){
+		float[] output = new float[1];
+		s.fetchSample(output, 0);
+		return output[0];
+	}
 }
