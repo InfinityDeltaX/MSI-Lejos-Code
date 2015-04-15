7d9e5b109d4f597071648f391f5104296858caf9
robert <infinitydeltax@gmail.com>
2015-04-14 20:24:21 -0400

    initial local?

---
+++ LEJOS/src/auto/Search.java
@@ -0,0 +1,24 @@
+package auto;
+
+import lejos.robotics.subsumption.Behavior;
+
+public class Search implements Behavior{
+
+	@Override
+	public boolean takeControl() {
+		return false;
+	}
+
+	@Override
+	public void action() {
+		
+	}
+
+	@Override
+	public void suppress() {
+		
+	}
+	
+	
+
+}
---
+++ LEJOS/src/auto/Sensors.java
@@ -0,0 +1,10 @@
+package auto;
+
+import lejos.hardware.port.Port;
+
+public class Sensors {
+	public final static Port one;
+	public final static Port two;
+	public final static Port three;
+	public final static Port four;
+}
